e606d8eda460cc535ff018eddd9fee7fa6dbc0f5
Chris Phu <chrisnguyenphu@gmail.com>
2023-11-23 12:51:32 -0600

    Small fixes and added persona name
    Command now returns the account's persona name as well for easy confirmation that the correct SteamID was supplied. Lots of small fixes in help flag grammar, refactoring several method names, and accepting I have named this project "over-x-playtime" so the counter method is now checking for > instead of >=. Still thinking about renaming this to 'steam-games-no-lesser-than-x-playtime'...

--- steam-games-over-x-playtime.py
+++ steam-games-over-x-playtime.py
@@ -6,7 +6,7 @@ steam_web_api_url = 'http://api.steampowered.com/'
 
 def check_for_help_flag():
     if len(sys.argv) == 1:
-        return
+        raise Exception('No arguments supplied.')
     if sys.argv[1] in ['-h', '--help']:
         print('\nCommand to run: py steam-games-over-x-playtime.py {steam web api key} {steamid} {x minutes}')
         print('\n- Go to https://steamcommunity.com/dev for Steam Web API Key')
@@ -15,9 +15,10 @@ def check_for_help_flag():
     return False
 
 def check_number_of_arguments():
-    if len(sys.argv) != 4:
-        quantifier = 'few' if len(sys.argv) < 4 else 'many'
-        raise Exception('Too ' + quantifier + ' arguments.')
+    expected_number_of_arguments = 4
+    if len(sys.argv) != expected_number_of_arguments:
+        quantifier = 'few' if len(sys.argv) < expected_number_of_arguments else 'many'
+        raise Exception('Too ' + quantifier + ' arguments supplied.')
 
 def check_x_minutes_argument(x_minutes):
     try:
@@ -25,14 +26,13 @@ def check_x_minutes_argument(x_minutes):
         return x_minutes
     except Exception as error:
         print(error)
-        raise Exception('Could not parse {x minutes} argument as in integer.')
+        raise Exception('Could not parse {x minutes} argument as an integer.')
 
 def write_url_argument(parameter, argument, first_argument = False):
     delimiter = '?' if first_argument else '&'
     return delimiter + parameter + '=' + argument
 
-def steam_web_api_GetOwnedGames(arguments):
-    url = steam_web_api_url + 'IPlayerService/GetOwnedGames/v0001/' + arguments
+def attempt_json_load_from_steam_web_api_url(url):
     try:
         response = urllib.request.urlopen(url)
         data = json.load(response)['response']
@@ -41,14 +41,25 @@ def steam_web_api_GetOwnedGames(arguments):
         print(error)
         raise Exception('Could not load .json data.')
 
+def steam_web_api_GetPlayerSummaries(arguments):
+    url = steam_web_api_url + 'ISteamUser/GetPlayerSummaries/v0002/' + arguments
+    return attempt_json_load_from_steam_web_api_url(url)
+
+def get_persona_name(data):
+    return data['players'][0]['personaname']
+
+def steam_web_api_GetOwnedGames(arguments):
+    url = steam_web_api_url + 'IPlayerService/GetOwnedGames/v0001/' + arguments
+    return attempt_json_load_from_steam_web_api_url(url)
+
 def get_game_count(data):
     return data['game_count']
 
-def get_game_count_with_playtime_equal_to_or_greater_than_x_minutes(data, x_minutes):
+def get_game_count_over_x_minutes_playtime(data, x_minutes):
     count = 0
     games = data['games']
     for game in games:
-        if game['playtime_forever'] >= x_minutes:
+        if game['playtime_forever'] > x_minutes:
             count += 1
     return count
 
@@ -58,17 +69,21 @@ def main():
     check_number_of_arguments()
     x_minutes = check_x_minutes_argument(sys.argv[3])
 
-    steam_web_api_key_argument = write_url_argument('key', sys.argv[1], True)
-    steamid_argument = write_url_argument('steamid', sys.argv[2])
-    format_argument = write_url_argument('format', 'json')
-    arguments = steam_web_api_key_argument + steamid_argument + format_argument
+    steam_web_api_key_url_argument = write_url_argument('key', sys.argv[1], True)
+    steamids_url_argument = write_url_argument('steamids', sys.argv[2])
+    steam_web_api_GetPlayerSummaries_url_arguments = steam_web_api_key_url_argument + steamids_url_argument
+    steam_web_api_GetPlayerSummaries_data = steam_web_api_GetPlayerSummaries(steam_web_api_GetPlayerSummaries_url_arguments)
+    persona_name = get_persona_name(steam_web_api_GetPlayerSummaries_data)
 
-    steam_web_api_GetOwnedGames_data = steam_web_api_GetOwnedGames(arguments)
+    steamid_url_argument = write_url_argument('steamid', sys.argv[2])
+    format_url_argument = write_url_argument('format', 'json')
+    steam_web_api_GetOwnedGames_url_arguments = steam_web_api_key_url_argument + steamid_url_argument + format_url_argument
+    steam_web_api_GetOwnedGames_data = steam_web_api_GetOwnedGames(steam_web_api_GetOwnedGames_url_arguments)
     game_count = get_game_count(steam_web_api_GetOwnedGames_data)
-    game_count_with_playtime_equal_to_or_greater_than_x_minutes = get_game_count_with_playtime_equal_to_or_greater_than_x_minutes(steam_web_api_GetOwnedGames_data, x_minutes)
+    game_count_over_x_minutes_playtime = get_game_count_over_x_minutes_playtime(steam_web_api_GetOwnedGames_data, x_minutes)
     
-    rounded_percentage_of_games = round((game_count_with_playtime_equal_to_or_greater_than_x_minutes / game_count) * 100, 1)
-    print('\n{} of {} games ({}%) with over {} minutes of playtime.'.format(game_count_with_playtime_equal_to_or_greater_than_x_minutes, game_count, rounded_percentage_of_games, x_minutes))
+    rounded_percentage_of_games = round((game_count_over_x_minutes_playtime / game_count) * 100, 1)
+    print('\n{} owns {} of {} games ({}%) with over {} minutes of playtime.'.format(persona_name, game_count_over_x_minutes_playtime, game_count, rounded_percentage_of_games, x_minutes))
 
 if __name__ == '__main__':
     main()
